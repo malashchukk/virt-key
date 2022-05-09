b0c67bed479d000cd8701be90233be6210e57aee
malashchuk <malashchuk.it@gmai.com>
2022-05-10 00:31:13 +0300

    feat: creat keyboard

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path =  require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    devServer:{
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test:/\.scss$/, 
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Keyboard',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+        new ESLintPlugin(),
+    ] 
+}
